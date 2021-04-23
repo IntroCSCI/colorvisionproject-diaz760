3f3859ff9b9eaf362d0dc061ad5eb6a740312f53
Alejandro Diaz-Diaz <>
2021-04-23 15:46:55 +0000

    p02 Second Deliverable

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-bool colorSame(string &);
+void colorSame(string &, vector<string>&);
 
 string colorTheme(vector<string>);
 
@@ -18,28 +18,14 @@ bool colorSearch=false;
 vector<string> colors;
 int itr3=0;
 
-ifstream myfile("colornames.txt");
+ifstream myfile("colorpatterns.txt");
 if (myfile.is_open()){
   while(getline(myfile,line)){
     colors.push_back(line);
   }
   myfile.close();
 }
-while(!colorSearch){
-
-cout<<"Input a color value with a given name to be saved with your theme: ";
-cin>> colorTitle;
-for(int i=0; i<colors.size(); i++){
-  if(colors[i]==colorTitle){
-    colorSearch=true;
-    break;
-  }
-
-}
-if(!colorSearch){
-  cout<<"Try again."<<endl;
-}
-}
+colorSame(colorTitle,colors);
 
 cout<<"Give a name to save the file as: " <<endl;
 cin>> fileTitle;
@@ -49,10 +35,10 @@ std::ofstream outFile (fileTitle);
 if (outFile.is_open()){
   outFile<<colorTitle<<endl;
   cout<<colorTitle<<endl;
-  for (int k=0;k<colors.size(); k++){
-    if(colors[k] != colorTitle){
-      outFile << colors[k] <<endl;
-      cout<< colors[k]<<endl;
+  for (int c=0;c<colors.size(); c++){
+    if(colors[c] != colorTitle){
+      outFile << colors[c] <<endl;
+      cout<< colors[c]<<endl;
       itr3++;
       if(itr3==5){
         break;
@@ -64,7 +50,21 @@ outFile.close();
 }
   return 0;
 }
-bool colorSame(){
+void colorSame(string & colorTitle,vector<string>& colors){
+bool colorSearch=false;
+  while(!colorSearch){
+    cout<<"Input a color value with a given name to be saved with your theme: "<< endl;
+    cin>> colorTitle;
+    for(int i=0; i<colors.size(); i++){
+      if(colors[i]==colorTitle){
+        colorSearch=true;
+        break;
+      }
+    }
+    if(!colorSearch){
+      cout<<"Try again."<<endl;
+    }
+  }
 }
 string colorTheme(vector<string> ){
 
